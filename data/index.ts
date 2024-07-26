97976fdfc69847cccb991e49280b52890d5c605a
Srinivas-559 <srinivasgollapalli597@gmail.com>
2024-07-26 19:31:13 +0530

    initial commmit

---
+++ data/index.ts
@@ -0,0 +1,257 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Highlights", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I focus on teamwork, promoting open and clear communication",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently developing blockchain solutions",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Decentralized Stable Coin ",
+      des: "Algorithically pegged stable coin",
+      img: "/DSC1.webp",
+      iconLists: ["/ethereum.png", "/solidity.png", "/foundry.jpeg", "/openzeppelin.png", "/chainlink.png"],
+      link: "https://github.com/Srinivas-559/Stable-Coin-DSC.git",
+    },
+    {
+      id: 2,
+      title: "Mood NFT - A dynamic NFT",
+      des: "Dynamic NFT , can switch mood ",
+      img: "/MoodNFT.jpg",
+      iconLists: ["/ethereum.png", "/solidity.png", "/foundry.jpeg", "/openzeppelin.png", "/chainlink.png"],
+      link: "https://github.com/Srinivas-559/Mood-NFT.git",
+    },
+    {
+      id: 3,
+      title: "Nature Saviors - Web App",
+      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+      img: "/NatureSaviors.jpg",
+      iconLists: ["/re.svg", "/tail.svg", "/js.png", "/next1.png", ],
+      link: "https://github.com/Srinivas-559/Next-Nature.git",
+    },
+    {
+      id: 4,
+      title: "Decentralized Lottery",
+      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+      img: "/Lottery.avif",
+      iconLists: ["/ethereum.png", "/solidity.png", "/foundry.jpeg", "/chainlink.png"],
+      link: "https://github.com/Srinivas-559/Decentralized-Lottery.git",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "From solving the Ethernaut challenges with Foundry scripts to developing a Decentralized Stable Coin (DSC), my journey through the blockchain world has been exciting. I've improved my skills in Solidity, creating secure and reliable smart contracts, and I'm always exploring new areas in decentralized technologies. Join me in shaping the future of blockchain!",
+      name: "Michael Johnson",
+      title: "Mastering Blockchain Technologies",
+    },
+    {
+      quote:
+        "Crafting seamless web applications is my forte. I've mastered the MERN stack, leveraging MongoDB, Express.js, React.js, and Node.js to create dynamic and responsive applications. Turning ideas into digital realities is my passion. Let's create the next big thing together!",
+      name: "Michael Johnson",
+      title: "MERN Stack Expretise",
+    },
+    {
+      quote:
+        "Algorithms are the heartbeats of efficient coding. My dedication to mastering data structures and algorithms is reflected in my daily problem-solving on platforms like LeetCode and HackerRank. This foundation empowers me to optimize and elevate my web and blockchain projects, ensuring top-notch performance and reliability. Let's unlock the potential of algorithmic brilliance!",
+      name: "Michael Johnson",
+      title: "Data Structures & Algorithms",
+    },
+    // {
+    //   quote:
+    //     "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    //   name: "Michael Johnson",
+    //   title: "Director of AlphaStream Technologies",
+    // },
+    // {
+    //   quote:
+    //     "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    //   name: "Michael Johnson",
+    //   title: "Director of AlphaStream Technologies",
+    // },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "Ethereum ",
+      img: "/ethereum.png",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "Chainlink",
+      img: "/chainlink1.png",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "Foundry",
+      img: "/foundry.jpeg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "Open-Zeppelin",
+      img: "/openzeppelin.png",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "Next.js",
+      img: "/nextjs.png",
+      nameImg: "/streamName.svg",
+    },
+   
+  ];
+
+export const education = [
+    {
+    id: 1,
+    specilization: "B.Tech in Computer Science Engineering",
+    institution : "RGUKT IIIT Ongole, Andhra Pradesh",
+    className: "md:col-span-2",
+    thumbnail: "",
+    graduationYear: "Expected Graduation: 2025",
+    cgpa:"CGPA: 8.6"     
+    },
+    {
+      id: 1,
+      specilization: "Intermediate (10+2)",
+      institution : "RGUKT IIIT Ongole, Andhra Pradesh",
+      className: "md:col-span-2",
+      thumbnail: "",
+      graduationYear: "2021",
+       cgpa:"CGPA: 9.6"
+            
+    },
+    {
+      id: 3,
+      specilization: "Secondary School Education (10th Grade))",
+      institution : "ZPHS , Undrajavaram, Andhra Pradesh",
+      className: "md:col-span-2",
+      thumbnail: "",
+      graduationYear: "2019",
+      cgpa:"CGPA: 10"
+    }
+  
+  ]
+
+
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "B.Tech in Computer Science Engineering",
+      desc: "RGUKT IIIT Ongole, Andhra Pradesh",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+      graduationYear: " 2021-2025",
+    },
+    {
+      id: 2,
+      title: "Intermediate (10+2)",
+      desc: "RGUKT IIIT Ongole, Andhra Pradesh",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+      graduationYear: "2019-2021",
+    },
+    {
+      id: 3,
+      title: "Secondary School Education (10th Grade)",
+      desc: "ZPHS , Undrajavaram, Andhra Pradesh",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+      graduationYear:"2019"
+    },
+    {
+      id: 4,
+      title: " Software Development(self-taught) ",
+      desc: "Through self-driven learning , I've mastered various programming languages ",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+      graduationYear:"2022-present"
+
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link:"https://github.com/Srinivas-559/"
+
+    },
+    
+    {
+      id: 3,
+      img: "/link.svg",
+      link:"https://www.linkedin.com/in/srinivas-gollapalli-3126a1242/"
+    },
+  ];
